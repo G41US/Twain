b63a90417014e61ecf50c68d4d276ac8358aaccc
Gaius Marvelous <90052557+G41US@users.noreply.github.com>
2025-11-25 13:58:49 +0100

    Update migrate.ts

--- api/migrate.ts
+++ api/migrate.ts
@@ -1,6 +1,14 @@
 import { execSync } from 'child_process';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req, res) {
-  const result = execSync('npx prisma db push', { encoding: 'utf8' });
-  res.status(200).send(result);
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const result = execSync('npx prisma db push', { encoding: 'utf8' });
+    res.status(200).send(result);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 }
