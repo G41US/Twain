0234006a06c4d29f2b16a2b37cd705fef866b01d
Gaius Marvelous <90052557+G41US@users.noreply.github.com>
2025-11-25 09:55:22 +0100

    Fix import path for authOptions in route.ts

--- app/api/swipe/right/route.ts
+++ app/api/swipe/right/route.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../auth/[...nextauth]/route";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
